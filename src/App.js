91a2126dc38baa319674f873d45c9178e895ca73
ss <ss@ssgpro.net>
2020-09-11 17:50:32 +0000

    updated the page message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sup!Hi there BROman!!!! Edit <code>src/App.js</code> and save to reload.
+          Hello this  is a test Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
